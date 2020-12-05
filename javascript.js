1d16396ca0b53b2c946cce0f93dac2eb869cfdbc
t0dida00 <t0dida00@students.oamk.fi>
2020-12-05 13:36:31 +0700

    05/12

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ function filter() {
 }
 function cancel() {
     document.getElementById("box-filter").style.display = "none";
-
+ 
 }
 action = "javascript:void(0);"
 
@@ -61,4 +61,54 @@ function down1(min) {
     }
     $("#total").text(parseInt(document.getElementById("moNey").value) * document.getElementById("myNumber").value +parseInt(document.getElementById("moNey2").value) * document.getElementById("myNumber2").value +parseInt(document.getElementById("moNey1").value) * document.getElementById("myNumber1").value )
 }
-window.onpaint = preloadFunc();
+//==========================
+
+    filterSelection("all")
+    function filterSelection(c) {
+      var x, i;
+      x = document.getElementsByClassName("filterDiv");
+      if (c == "all") c = "";
+      for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+      }
+ 
+    }
+    
+    function w3AddClass(element, name) {
+      var i, arr1, arr2;
+      
+      arr1 = element.className.split(" ");
+      
+      arr2 = name.split(" ");
+      console.log(name);
+    
+      for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+      }
+      
+    }
+    
+    function w3RemoveClass(element, name) {
+      var i, arr1, arr2;
+      arr1 = element.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+          arr1.splice(arr1.indexOf(arr2[i]), 1);     
+        }
+      }
+      element.className = arr1.join(" ");
+    
+    }
+function submit1()
+{
+    document.getElementById("submit-button").style.display = "block";
+
+}
+function modal1()
+{
+    document.getElementById("successful").style.display = "block";
+    document.getElementById("customer-form").style.display = "none";
+    document.getElementById("customer").style.margin = "0";
+}
